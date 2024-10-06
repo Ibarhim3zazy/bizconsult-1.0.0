69388061a446c21f3417235fd06aad165a238bb5
Ibrahim Azazy <ebrahim3zazy@gmail.com>
2024-10-06 04:22:41 +0300

    update setting service provider

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider
@@ -20,7 +22,11 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $settings = Setting::findOrFail(1);
-        view()->share('settings', $settings);
+        if (Schema::hasTable('settings') && Setting::exists()) {
+            $settings = Setting::findOrFail(1);
+            view()->share('settings', $settings);
+        } else {
+            Log::warning('Settings table not found. Please run php artisan db:seed --class=SettingTableSeeder');
+        }
     }
 }
